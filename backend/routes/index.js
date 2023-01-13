35d4b67172d891127866ad52b5d03b2054adf380
curtis-culpepper <107226325+curtis-culpepper@users.noreply.github.com>
2023-01-12 19:35:51 -0500

    finished initializing express and sequelize and settings initial protections

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/hello/world", function (req, res) {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.send("Hello World!");
+// this allows any dev to reset the CSRF token cookie
+// needs to be removed from production once frontend is implimented
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        "XSRF-Token": csrfToken,
+    });
 });
+// ...
 
 module.exports = router;
