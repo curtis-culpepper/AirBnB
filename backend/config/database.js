035ab1ccd0ae3c34afc1fc01079f11f29b0a9994
curtis-culpepper <107226325+curtis-culpepper@users.noreply.github.com>
2023-01-21 13:56:40 -0500

    corrected errors to enable deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,6 +7,7 @@ module.exports = {
         seederStorage: "sequelize",
         logQueryParameters: true,
         typeValidation: true,
+        logging: true,
     },
     production: {
         use_env_variable: "DATABASE_URL",
